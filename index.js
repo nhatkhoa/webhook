f372b9b6b5a91cf143da56bbb6c835d6aa4813a8
khoahoang <khoahoang.sa@gmail.com>
2017-01-16 16:23:16 +0700

    set webhook example

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const {items} = require('./data.json')
+
+const getJobs = (data) => {
+    if (data.action !== 'kabo.getJobs')
+        return
+    return items;
+}
+
+app.get('/', function(req, res) {
+    res.send('Hello World')
+})
+
+app.get('/webhook', function(req, res) {
+    req.body = {
+        action: 'kabo.getJobs'
+    }
+    console.log(req.body)
+    const data = getJobs(req.body).map((item) => item.link)
+    const responseData = {
+        "speech": "",
+        "displayText": JSON.stringify(data),
+        "data": {
+        },
+        "contextOut": "",
+        "source": "KMS"
+    }
+    res.send(responseData)
+})
+
+app.listen(8888)
